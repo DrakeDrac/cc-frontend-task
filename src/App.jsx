f13f0ea14584ba6a6536dccce19a5851fd37b3ea
drakedrac <sidhuyash2006@gmail.com>
2025-08-24 18:24:36 +0530

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,107 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import TaskList from './components/TaskList'
-import {v4} from 'uuid';
+import { v4 } from 'uuid';
 
 function App() {
 
-  const [taskList, setTaskList] = useState(()=>JSON.parse(localStorage.getItem("tasks") || '[]'))
+  const [taskList, setTaskList] = useState(() => JSON.parse(localStorage.getItem("tasks") || '[]'))
+  const [selectedTaskList, setSelectedTaskList] = useState(0)
+  const [editingHeading, setEditingHeading] = useState(null)
 
   useEffect(() => {
-    localStorage.setItem("tasks",JSON.stringify(taskList))
+    // everytime tasklist is updated, we save it
+    localStorage.setItem("tasks", JSON.stringify(taskList))
   }, [taskList])
-  
+
+  useEffect(() => {
+    if (taskList.length == 0)
+      setTaskList([
+    { "heading": "This is a Task list", "id": "10becb9f-8d25-4fa6-a9cc-7e1659fa17bc", "list": [{ "id": "96b2c212-9316-43eb-9c60-76541a9adcd3", "text": "This is a task", "done": false }, { "id": "cceb6464-f7bf-41e0-b796-94ab39286492", "text": "double click me to edit", "done": false }] },
+    { "heading": "to create new tasks,", "id": "d645f15b-a1ae-4086-ad57-80d6a72f5e96", "list": [{ "id": "20ae731f-10b2-4da6-87af-3608ed255b9b", "text": "add your tasks here", "done": false }] },
+    { "heading": "Use the button below ", "id": "4e223c4a-8cf5-4613-afbb-96ba38441754", "list": [{ "id": "e0d650c5-f146-4e0a-a786-2f3dff0c624b", "text": "add your tasks here", "done": false }]},
+    { "heading":"Double click to edit","id":"8ee7fd8c-684e-4fea-a455-067e7ef0455e","list":[{"id":"e812a322-7d9e-4cc6-9ce5-da9babad494b","text":"hehe","done":false}]}]);
+  })
+
 
   const createTaskList = () => {
-    setTaskList([{"heading":"Task", "id":v4(),"list":[]},...taskList])
+    setTaskList([{ "heading": "Task", "id": v4(), "list": [] }, ...taskList])
   }
 
-  function TaskListElem ({data}){
-    return(
-      <div>{data.heading}</div>
+  const updateTaskList = (updatedTask) => {
+    setTaskList(prev => {
+      const updated = [...prev];
+      updated[selectedTaskList] = updatedTask;
+      return updated;
+    })
+  }
+
+  const updateTaskListHeading = (newHeading) => {
+    setTaskList(prev => {
+      let updated = [...prev];
+      if (updated[selectedTaskList]) {
+        updated[selectedTaskList] = { ...updated[selectedTaskList], heading: newHeading };
+      }
+      return updated;
+    })
+    setEditingHeading(null)
+  }
+
+  function TaskListElem({ data, index }) {
+
+    const [newTaskListName, setNewTaskListName] = useState("");
+
+    const isEditing = editingHeading === index
+
+    const handleDoubleClick = () => {
+      setEditingHeading(index)
+      setNewTaskListName(data.heading);
+    }
+
+    const deleteTaskList = (index) => {
+      let updatedList = [];
+      for (let i = 0; i < taskList.length; i++) {
+        let e = taskList[i];
+        if (i !== index) {
+          updatedList.push(e);
+        }
+      }
+      setTaskList(updatedList);
+    }
+
+    const handleSubmit = (e, useE = true) => {
+      if (useE)
+        updateTaskListHeading(e.target.value)
+      else
+        updateTaskListHeading(newTaskListName)
+    }
+
+    return (
+      <div
+        className={`taskListItem ${index === selectedTaskList ? 'selected' : ''}`}
+        onClick={() => setSelectedTaskList(index)}
+      >
+        {isEditing ? (
+          <input
+            type="text"
+            defaultValue={data.heading}
+            onBlur={handleSubmit}
+            autoFocus
+            className="editingHeading"
+            onChange={(e) => setNewTaskListName(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key == "Enter") handleSubmit({}, false)
+            }}
+          />
+        ) : (
+          <>
+            <div onDoubleClick={handleDoubleClick}>{data.heading}</div>
+            <span onClick={() => deleteTaskList(index)} className="material-symbols-outlined">
+              delete
+            </span>
+          </>
+        )}
+      </div>
     )
   }
 
@@ -27,14 +110,19 @@ function App() {
       <div className='sideBar'>
         <div className='list'>
           {
-                taskList.map((e,i)=><TaskListElem key={i} data={e}></TaskListElem>)
+            taskList.map((e, i) => <TaskListElem key={i} data={e} index={i}></TaskListElem>)
           }
         </div>
+
         <div className='listButtons'>
-          <button className='createListButton' onClick={createTaskList}> Create List</button>
+          <button className='createListButton' onClick={createTaskList}> Create New List</button>
         </div>
       </div>
-      <TaskList task={taskList[0] || {"heading":"moja", "list":[{"text": "okok", "done": false},{"text": "mojmojS","done": true}]}}></TaskList>
+
+      <TaskList
+        task={taskList[selectedTaskList]}
+        onUpdateTask={updateTaskList}
+      ></TaskList>
     </>
   )
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,24 +1,147 @@
 import { useState } from "react"
+import { v4 } from "uuid";
 
-function Task({data}){
-    const [checked, setChecked]=useState(data.done);
-    return(
-        <div className="task"><input checked={checked} onChange={(e)=>{setChecked(e.currentTarget.checked)}} type="checkbox"/>{data.text}</div>
+function Task({ data, onChange, onUpdateText, onDeleteTask }) {
+    const [checked, setChecked] = useState(data.done);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editText, setEditText] = useState(data.text);
+
+    const handleTaskChange = (e) => {
+        let newChecked = e.currentTarget.checked;
+        setChecked(newChecked);
+        onChange(data.id, newChecked);
+    }
+
+    const handleDoubleClick = () => {
+        setIsEditing(true);
+        setEditText(data.text);
+    }
+
+
+    const handleSave = () => {
+        if (editText != "") {
+            onUpdateText(data.id, editText);
+        }
+        setIsEditing(false);
+    }
+
+    return (
+        <div className="task">
+            <input checked={checked} onChange={handleTaskChange} type="checkbox" />
+            {isEditing ? (
+                <input
+                    type="text"
+                    value={editText}
+                    onChange={(e) => setEditText(e.target.value)}
+                    onBlur={handleSave}
+                    autoFocus
+                    className="editingTask"
+                />
+            ) : (
+                <div className="taskText">
+                    <div onDoubleClick={handleDoubleClick}>{data.text}</div>
+                    <span onClick={() => onDeleteTask(data.id)} className="material-symbols-outlined">
+                        delete
+                    </span>
+                </div>
+            )}
+        </div>
     )
 }
 
-export default function taskList({task}) {
+export default function taskList({ task, onUpdateTask }) {
+    const [newTaskText, setNewTaskText] = useState("");
+    if (!task) return null;
+
+    const addTask = () => {
+        if (newTaskText == "") return;
+
+        let newTask = {
+            id: v4(),
+            text: newTaskText,
+            done: false
+        };
+
+        let updatedTask = {
+            ...task,
+            list: [...task.list, newTask]
+        };
+
+        onUpdateTask(updatedTask);
+        setNewTaskText("");
+    };
+
+    const deleteTask = (id) => {
+        console.log(id)
+        let updatedTasks = [];
+        for (let i = 0; i < task.list.length; i++)
+            if (task.list[i].id != id)
+                updatedTasks.push(task.list[i])
+        task.list = updatedTasks;
+        onUpdateTask(task);
+    }
+
+    const handleToggleTask = (id, newDone) => {
+        let updatedList = [];
+        for (let i = 0; i < task.list.length; i++) {
+            let e = task.list[i];
+            if (e.id == id) {
+                updatedList.push({ ...e, done: newDone });
+            } else {
+                updatedList.push(e);
+            }
+        }
+        let updatedTask = {
+            ...task,
+            list: updatedList
+        };
+        onUpdateTask(updatedTask);
+    };
+
+    const handleUpdateTaskText = (id, newText) => {
+        let updatedList = [];
+        for (let i = 0; i < task.list.length; i++) {
+            let e = task.list[i];
+            if (e.id == id) {
+                updatedList.push({ ...e, text: newText });
+            } else {
+                updatedList.push(e);
+            }
+        }
+        let updatedTask = {
+            ...task,
+            list: updatedList
+        };
+        onUpdateTask(updatedTask);
+    };
+
     return (
         <div className='taskList'>
             <h2>
-                <input type="checkbox"/>
-                {task.heading}
+                {task?.heading}
             </h2>
 
-            <hr/>
-            {
-                task.list.map((e,i)=><Task key={i} data={e}></Task>)
-            }
+            <hr />
+            <div className="tasksContainer">
+                {
+                    task.list.map((e, i) => <Task key={e.id || i} data={e} onDeleteTask={deleteTask} onChange={handleToggleTask} onUpdateText={handleUpdateTaskText}></Task>)
+                }
+            </div>
+
+            <div className="addTaskContainer">
+                <input
+                    type="text"
+                    className="addTaskContainerText"
+                    placeholder="Add a new task..."
+                    value={newTaskText}
+                    onKeyDown={(e) => {
+                        console.log(e);
+                        if(e.key=="Enter") addTask()
+                        }}
+                    onChange={(e) => setNewTaskText(e.target.value)}
+                />
+                <button className="addTaskContainerButton" onClick={addTask}>Add Task</button>
+            </div>
         </div>
     )
 }
