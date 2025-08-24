c0490880f76721b267758a4abe9d95192123cde7
drakedrac <sidhuyash2006@gmail.com>
2025-08-24 15:00:26 +0530

    tried localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import TaskList from './components/TaskList'
+import {v4} from 'uuid';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [taskList, setTaskList] = useState(()=>JSON.parse(localStorage.getItem("tasks") || '[]'))
+
+  useEffect(() => {
+    localStorage.setItem("tasks",JSON.stringify(taskList))
+  }, [taskList])
+  
+
+  const createTaskList = () => {
+    setTaskList([{"heading":"Task", "id":v4(),"list":[]},...taskList])
+  }
+
+  function TaskListElem ({data}){
+    return(
+      <div>{data.heading}</div>
+    )
+  }
 
   return (
     <>
       <div className='sideBar'>
         <div className='list'>
+          {
+                taskList.map((e,i)=><TaskListElem key={i} data={e}></TaskListElem>)
+          }
         </div>
         <div className='listButtons'>
+          <button className='createListButton' onClick={createTaskList}> Create List</button>
         </div>
       </div>
-      <TaskList task={{"heading":"moj", "list":[{"text": "okok"},{"text": "mojmojS"}]}}></TaskList>
+      <TaskList task={taskList[0] || {"heading":"moja", "list":[{"text": "okok", "done": false},{"text": "mojmojS","done": true}]}}></TaskList>
     </>
   )
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react"
+
 function Task({data}){
+    const [checked, setChecked]=useState(data.done);
     return(
-        <div><input type="checkbox"/>{data.text}</div>
+        <div className="task"><input checked={checked} onChange={(e)=>{setChecked(e.currentTarget.checked)}} type="checkbox"/>{data.text}</div>
     )
 }
 
 export default function taskList({task}) {
     return (
         <div className='taskList'>
-            <h2><input type="checkbox"/>{task.heading}</h2>
-            <hr></hr>
+            <h2>
+                <input type="checkbox"/>
+                {task.heading}
+            </h2>
+
+            <hr/>
             {
                 task.list.map((e,i)=><Task key={i} data={e}></Task>)
             }
